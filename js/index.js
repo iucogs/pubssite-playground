c52454437a4dd789d0d8513930617b0f6f5347e5
Pramod Sripada <pramod.sripada@gmail.com>
2016-06-06 20:01:36 -0400

    Added templating logic

--- js/index.js
+++ js/index.js
@@ -1,39 +1,134 @@
-$('#add').on('click', function(){
-   //$('#contributors-table tr:last').after(tableRow);
-  //$('#contributors-table').append(tableRow);
-/*   var table=document.getElementById('contributors-table');
-  var tableRow = table.rows[1].cloneNode(true);
-  $('#contributors-table > tbody:last').append(tableRow); 
-  $("#contributors-table tr:last").clone().find('input').val('').end().insertAfter("#contributors-table tr:last"); */
+$("document").ready(function () {
+  console.log("In the doc ready func")
+  $.getJSON('http://nupubs.cogs.indiana.edu/citation/33590', function (data) {
+    var template = $("#dataTemplate").html();
+    var result = Mustache.render(template, data);
+    $("#templateForm").html(result);
+
+    // Setting the publicationType
+    var pubType = data.pubtype.toLowerCase();
+    $("#pubtype option[value="+pubType+"]").attr("selected", true);
+
+    switch(pubType) {
+      case "article": 
+        console.log("Article")
+        var dynamicTemplate = $("#articleTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "book":
+        console.log("Book")
+        var dynamicTemplate = $("#bookTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "techreport": 
+        console.log("Tech Report")
+        var dynamicTemplate = $("#techreportTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "misc":
+        console.log("Misc")
+        var dynamicTemplate = $("#miscTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "proceedings": 
+        console.log("proceedings")
+        var dynamicTemplate = $("#proceedingsTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "unpublished":
+        console.log("unpublished")
+        var dynamicTemplate = $("#unpublishedTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "phdthesis":
+      case "mastersthesis":
+        console.log("thesis")
+        var dynamicTemplate = $("#thesisTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "web_published":
+        console.log("web_published")
+        var dynamicTemplate = $("#webpublishedTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "incollection":
+      case "inproceedings":
+      case "conference":
+        console.log("Incollection || inproceedings || conference")
+        var dynamicTemplate = $("#incollectionTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "manualTemplate":
+        console.log("manualTemplate")
+        var dynamicTemplate = $("#manualTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break; 
+
+    } 
+
+    // Setting the month
+    var publicationMonth = data.month.toLowerCase();
+    if(data.month.toLowerCase() == "") {
+      $('#month option[value="----"]').attr('selected',true);
+    } else {
+      $('#month option[value='+publicationMonth+']').attr('selected',true);
+    }
+
+  });
+  
+});
+
+
+$(document).on("click", "#add", function (){
+  console.log("In the addition function");
   var tableRow = `
-<tr>
-						<td>
-							<label class="checkbox-label">
-								<div class="radio">
-									<input type="radio" name="optradio" class="table-radio">
-								</div>
-							</label>
-						</td>
-						<td>
-							<div class="form-group">
-								<select class="form-control input-sm" class="contributor-select">
-									<option value="author">Author</option>
-			    					<option value="editor">Editor</option>
-			    					<option value="translator">Translator</option>
-								</select>
-							</div>
-						</td>
-						<td>
-							<div class="form-group">
-								<input class="form-control input-sm name" type="text">
-							</div>
-						</td>
-						</tr>
+          <tr>
+            <td>
+              <label class="checkbox-label">
+                <div class="radio">
+                  <input type="radio" name="optradio" class="table-radio">
+                </div>
+              </label>
+            </td>
+            <td>
+              <div class="form-group">
+                <select class="form-control input-sm" class="contributor-select">
+                  <option value="author">Author</option>
+                    <option value="editor">Editor</option>
+                    <option value="translator">Translator</option>
+                </select>
+              </div>
+            </td>
+            <td>
+              <div class="form-group">
+                <input class="form-control input-sm name" type="text">
+              </div>
+            </td>
+            </tr>
 `
    $('#contributors-table > tbody:last').append(tableRow); 
 });
 
-$("#delete").click(function () {
+$(document).on("click", "#delete", function () {
   $(".selected").remove();
     $("#contributors-table tr").each(function () {
       $('td', this).each(function () {
@@ -43,7 +138,18 @@ $("#delete").click(function () {
       })
       
   });  
-})
+});
+
+$(document).on("click", "#up", function () {
+  console.log($(".selected").find("input.name").val());
+  $(".selected").prev().before($(".selected"));
+});
+
+$(document).on("click", "#down", function () {
+  console.log($(".selected").find("input.name").val());
+  $(".selected").next().after($(".selected"));
+});
+
 
 $(document).on("click", "#contributors-table tr", function(event) {
   if($(event.target).is('[type="radio"]')){
@@ -51,18 +157,9 @@ $(document).on("click", "#contributors-table tr", function(event) {
    $(this).addClass("selected").siblings().removeClass("selected");
      }
 });
- 
-$("#up").click(function () {
-  console.log($(".selected").find("input.name").val());
-  $(".selected").prev().before($(".selected"));
-});
 
-$("#down").click(function () {
-  console.log($(".selected").find("input.name").val());
-  $(".selected").next().after($(".selected"));
-})
+$("#templateForm").submit(function (e) {
 
-$("#templateForm").submit(function () {
    var authors = [];
     $("#contributors-table tr").each(function () {
       $('td', this).each(function () {
@@ -123,4 +220,5 @@ var outputJSON = {
   "organization": ""
 };
   console.log(outputJSON);
+  e.stopImmediatePropagation();
 });
