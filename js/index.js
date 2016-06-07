617f87e33f80a650da8adf3007c094ce63e2524e
Pramod Sripada <pramod.sripada@gmail.com>
2016-06-07 10:02:49 -0400

    Templating logic for dropdown on change

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
+var globalData = {};
 $("document").ready(function () {
   console.log("In the doc ready func")
   $.getJSON('http://nupubs.cogs.indiana.edu/citation/33590', function (data) {
+    globalData = data;
     var template = $("#dataTemplate").html();
     var result = Mustache.render(template, data);
     $("#templateForm").html(result);
@@ -150,12 +152,92 @@ $(document).on("click", "#down", function () {
   $(".selected").next().after($(".selected"));
 });
 
-
 $(document).on("click", "#contributors-table tr", function(event) {
   if($(event.target).is('[type="radio"]')){
          console.log("In this function");
    $(this).addClass("selected").siblings().removeClass("selected");
      }
+});
+
+$(document).on("change", "#pubtype", function () {
+  var pubType = this.value.toLowerCase();
+  var data = globalData;
+  switch(pubType) {
+      case "article": 
+        console.log("Article")
+        var dynamicTemplate = $("#articleTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "book":
+        console.log("Book")
+        var dynamicTemplate = $("#bookTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "techreport": 
+        console.log("Tech Report")
+        var dynamicTemplate = $("#techreportTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "misc":
+        console.log("Misc")
+        var dynamicTemplate = $("#miscTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "proceedings": 
+        console.log("proceedings")
+        var dynamicTemplate = $("#proceedingsTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "unpublished":
+        console.log("unpublished")
+        var dynamicTemplate = $("#unpublishedTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "phdthesis":
+      case "mastersthesis":
+        console.log("thesis")
+        var dynamicTemplate = $("#thesisTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "web_published":
+        console.log("web_published")
+        var dynamicTemplate = $("#webpublishedTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "incollection":
+      case "inproceedings":
+      case "conference":
+        console.log("Incollection || inproceedings || conference")
+        var dynamicTemplate = $("#incollectionTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break;
+
+      case "manualTemplate":
+        console.log("manualTemplate")
+        var dynamicTemplate = $("#manualTemplate").html();
+        var dynamicResult = Mustache.render(dynamicTemplate, data);
+        $("#dynamicUpdate").html(dynamicResult);
+        break; 
+
+    } 
+
 });
 
 $("#templateForm").submit(function (e) {
